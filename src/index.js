c2c6f636f9ac0a336e80e2f85581b9355a8b20a7
Cusii <davidcusi14@gmail.com>
2020-11-19 23:52:35 -0300

    Arreglo de compatibilidad de codigo

--- src/index.js
+++ src/index.js
@@ -1,51 +1,42 @@
 let homePage = require('./homePage')
 const sucursales = require("./sucursales")
-const enCartelera = require('./enCartelera') <<
-    << << < HEAD
+const enCartelera = require('./enCartelera')
+
 const preFrecuentes = require('./preguntasFrecuentes')
-
-let movies = homePage.leerJSON()
-let teatros = sucursales.leerJSON()
-let cartelera = enCartelera.leerJSON()
-let preguntasFrecuentes = preFrecuentes.leerJSON()
-
-
-===
-=== =
 const masVotadas = require("./masVotadas")
 
 let movies = homePage.leerJSON()
 let teatros = sucursales.leerJSON()
 let cartelera = enCartelera.leerJSON()
-let votadas = masVotadas.leerJSON() >>>
-    >>> > c8c4eb5c72e6f130b60e5b108bfd8a24e7f6f3f4
+let votadas = masVotadas.leerJSON()
+let preguntasFrecuentes = preFrecuentes.leerJSON()
 
 
 
 module.exports = {
-        homePage: function(req, res) {
-            res.write("​Bienvenidos a DH Movies el mejor sitio para encontrar las mejores películas, incluso mucho mejor que Netflix, Cuevana y PopCorn​.")
-            res.write("\n\n")
-            res.write("---------")
-            res.write("LA CANTIDAD DE PELICULAS QUE TENEMOS SON : " + movies.movies.length)
-            res.write("---------")
-            res.write("\n\n")
+    homePage: function(req, res) {
+        res.write("​Bienvenidos a DH Movies el mejor sitio para encontrar las mejores películas, incluso mucho mejor que Netflix, Cuevana y PopCorn​.")
+        res.write("\n\n")
+        res.write("---------")
+        res.write("LA CANTIDAD DE PELICULAS QUE TENEMOS SON : " + movies.movies.length)
+        res.write("---------")
+        res.write("\n\n")
 
-            movies.movies.sort(function(a, b) {
-                if (a.title > b.title) {
-                    return 1;
-                }
-                if (a.title < b.title) {
-                    return -1;
-                }
-                // a must be equal to b
-                return 0;
-            });
-            movies.movies.forEach(movie => {
-                res.write("*" + movie.title + '\n\n')
+        movies.movies.sort(function(a, b) {
+            if (a.title > b.title) {
+                return 1;
+            }
+            if (a.title < b.title) {
+                return -1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+        movies.movies.forEach(movie => {
+            res.write("*" + movie.title + '\n\n')
 
-            });
-            res.write(`     
+        });
+        res.write(`     
                        -En Cartelera(/en-cartelera)
                        -Mas Votadas(/mas-votadas)
                        -Sucursales(/sucursales)
@@ -55,43 +46,42 @@ module.exports = {
 
 
 
-            res.end()
-        },
-        enCartelera: function(req, res) {
-            res.write("En Cartelera")
+        res.end()
+    },
+    enCartelera: function(req, res) {
+        res.write("En Cartelera")
+        res.write("\n\n")
+        res.write("Cantidad de pelicula " + movies.movies.length)
+        res.write("\n\n")
+        movies.movies.forEach(movie => {
+            res.write("---------")
+            res.write(movie.title)
+            res.write("---------")
             res.write("\n\n")
-            res.write("Cantidad de pelicula " + movies.movies.length)
+            res.write(movie.overview + '\n')
             res.write("\n\n")
-            movies.movies.forEach(movie => {
-                res.write("---------")
-                res.write(movie.title)
-                res.write("---------")
-                res.write("\n\n")
-                res.write(movie.overview + '\n')
-                res.write("\n\n")
-            });
-            res.end()
-        },
+        });
+        res.end()
+    },
 
-        sucursales: function(req, res) {
-            teatros.theaters.forEach(teatro => {
-                res.write(`${"·"}${teatro.name}${"·"}\n\n${"-"}${teatro.address}\n\n${"-"}${teatro.description}\n${teatro.total_rooms}\n\n`)
+    sucursales: function(req, res) {
+        teatros.theaters.forEach(teatro => {
+            res.write(`${"·"}${teatro.name}${"·"}\n\n${"-"}${teatro.address}\n\n${"-"}${teatro.description}\n${teatro.total_rooms}\n\n`)
 
 
-            })
-            res.end()
+        })
+        res.end()
 
 
-        },
-        <<
-        << << < HEAD
-        contacto: function(req, res) {
-            res.write(`
+    },
+
+    contacto: function(req, res) {
+        res.write(`
                     ++++++++++++++++++
                         Contactanos
                     ++++++++++++++++++`)
-            res.write("\n\n\n\n\n")
-            res.write(`
+        res.write("\n\n\n\n\n")
+        res.write(`
         
         ¿Tenés algo para contarnos? Nos encanta escuchar a nuestros
         clientes. Si deseas contactarnos podés escribirnos al siguiente email:
@@ -99,35 +89,31 @@ module.exports = {
         sugerencia o reclamo y será respondido a la brevedad posible. Recordá que
         también podes consultar la sección de Preguntas Frecuentes para obtener
         respuestas inmediatas a los problemas más comunes.`)
-            res.end()
-        },
-        prefuntasFrecuentes: function(req, res) {
-            res.write("Preguntas Frecuentes")
+        res.end()
+    },
+    prefuntasFrecuentes: function(req, res) {
+        res.write("Preguntas Frecuentes")
+        res.write("\n\n")
+        res.write("- El total de preguntas es: " + preguntasFrecuentes.faqs.length)
+        preguntasFrecuentes.faqs.forEach(element => {
             res.write("\n\n")
-            res.write("- El total de preguntas es: " + preguntasFrecuentes.faqs.length)
-            preguntasFrecuentes.faqs.forEach(element => {
-                res.write("\n\n")
-                res.write("Pregunta: " + element.faq_title)
-                res.write("\n\n")
-                res.write("Respuesta: " + element.faq_answer)
-
-            });
-
-            res.end()
-
-        },
-        masVotadas: function(req, res) {
-            res.write("---Mas Votadas---")
+            res.write("Pregunta: " + element.faq_title)
             res.write("\n\n")
-            res.write("Cantidad de pelicula " + masVotadas.mayor7().length)
-            res.write("\n\n")
-            res.write("El rating promedio es " + masVotadas.ratinAvg())
-            res.end()
-        }
+            res.write("Respuesta: " + element.faq_answer)
 
-    } ===
-    === =
+        });
 
+        res.end()
+
+    },
+    masVotadas: function(req, res) {
+        res.write("---Mas Votadas---")
+        res.write("\n\n")
+        res.write("Cantidad de pelicula " + masVotadas.mayor7().length)
+        res.write("\n\n")
+        res.write("El rating promedio es " + masVotadas.ratinAvg())
+        res.end()
+    },
     masVotadas: function(req, res) {
         res.write("---Mas Votadas---")
         res.write("\n\n")
@@ -137,6 +123,4 @@ module.exports = {
         res.end()
     }
 
-
-} >>>
->>> > c8c4eb5c72e6f130b60e5b108bfd8a24e7f6f3f4
+}
